6ea24d4c13a5b1dd50fa54dac19f1eb0e93dcc88
Noah Yin <noah.yin@berkeley.edu>
2022-04-14 18:05:39 -0700

    commit first

---
+++ Fitspo/Fitspo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fitspo
+//
+//  Created by Noah Yin on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
